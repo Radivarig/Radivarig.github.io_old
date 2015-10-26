468319347c66da2300b6198f167e93dbc84ed900
Radivarig <reslav.hollos@gmail.com>
2015-10-26 00:10:33 +0000

    (prepared) landing page

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,17 +8,30 @@ var App = React.createClass({
     chg[name] = e.target ? e.target.value : e
     this.setState(chg)
   }
+, componentDidMount: function() {
+    document.body.style.backgroundColor = '#C7D1CE'
+  }
 , render: function() {
+    var self = this
     return (
       <div>
-        test
+        <div style={{textAlign: 'center'}}>
+
+          <hr/>
+
+          <h3>Name of the project</h3>
+
+          <hr/>
+        </div>
+
         <RouteHandler/>
+
       </div>
     )
   }
 })
 
-    //<DefaultRoute name="demo" handler={Demo}/>
+//    <DefaultRoute name="demo" handler={Demo}/>
 var routes = (
   <Route name="app" path="/" handler={App}>
   </Route>
