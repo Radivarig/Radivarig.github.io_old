20fbdac160bff2fb2dc1473ead43d6b5cbfa432e
Radivarig <reslav.hollos@gmail.com>
2015-11-02 13:53:38 +0100

    (add) 404 letters

--- src/routes.jsx
+++ src/routes.jsx
@@ -198,8 +198,9 @@ var NotFoundViewer = React.createClass({
   render: function() {
     return (
       <div style={{textAlign: 'center'}}>
-      ?
+      <span style={{fontSize: '260px'}}>4</span>
       <img style={{width: 200, height: 200}} src='http://i.imgur.com/0opRxl2.gif' />
+      <span style={{fontSize: '260px'}}>4</span>
       </div>
     )
   }
