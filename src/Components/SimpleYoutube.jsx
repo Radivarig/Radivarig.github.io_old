a3fa5f67b7ee0b6898c38416d1235a1ac8a91dfb
Radivarig <reslav.hollos@gmail.com>
2018-03-19 00:02:54 +0100

    Change to auto width

--- src/Components/SimpleYoutube.jsx
+++ src/Components/SimpleYoutube.jsx
@@ -4,8 +4,7 @@ import YouTube from "react-youtube"
 export default class SimpleYoutube extends React.Component {
   render = () => {
     const opts = {
-      "height": "390",
-      "width": "640",
+      "width": "100%",
       "playerVars": { // https://developers.google.com/youtube/player_parameters
         "autoplay": 1,
       },
