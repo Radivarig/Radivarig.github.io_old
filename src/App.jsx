b7bd634116c07f327a05ec74e39ac95161eedf61
Radivarig <reslav.hollos@gmail.com>
2018-03-18 02:21:21 +0100

    Add theme wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -5,110 +5,118 @@ import { HashLink as Link } from "react-router-hash-link"
 import ProfileViewer from "./Components/ProfileViewer.jsx"
 import NotFoundViewer from "./Components/NotFoundViewer.jsx"
 import StatCounter from "./Components/StatCounter.jsx"
+import TabDetails from "./Components/TabDetails.jsx"
 
 import GithubRibbon from "./Components/GithubRibbon.jsx"
 
 import { Grid, Button, Hidden } from "material-ui"
-
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles"
+const theme = createMuiTheme ({
+  "palette": {
+    // "type": "dark",
+  },
+})
 import tabInfos from "./tabInfos.js"
 
 import "style-loader!css-loader!./index.css"
 
 export default class App extends React.Component {
   render = () => (
-    <BrowserRouter>
-      <Route render={(props) => {
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Route render={(props) => {
 
-        const routes = tabInfos.map ((tab, i) => {
-          const buttonStyle = {
-            "padding": 0,
-            "minHeight": 0,
-          }
-          return (
-            <Link
-              to={`#${tab.route}`}
-              key={i}
-            >
-              <Button style={buttonStyle}>{tab.label}</Button>
-            </Link>
-          )
-        })
+          const routes = tabInfos.map ((tab, i) => {
+            const buttonStyle = {
+              "padding": 0,
+              "minHeight": 0,
+            }
+            return (
+              <Link
+                to={`#${tab.route}`}
+                key={i}
+              >
+                <Button style={buttonStyle}>{tab.label}</Button>
+              </Link>
+            )
+          })
 
-        const contents = tabInfos.map ((tab, i) => {
-          const isActive = props.location.hash === `#${tab.route}`
+          const contents = tabInfos.map ((tab, i) => {
+            const isActive = props.location.hash === `#${tab.route}`
 
-          let content =
-            <div
-              key={i}
-              id={tab.route}
-            >
-              {tab.label}
-            </div>
-
-          if (isActive)
-            content =
-              <div key={i} className="highlight">
-                {content}
+            let content =
+              <div
+                key={i}
+                id={tab.route}
+              >
+                <TabDetails tab={tab} />
               </div>
 
-          return content
+            if (isActive)
+              content =
+                <div key={i} className="highlight">
+                  {content}
+                </div>
 
-        })
+            return content
 
-        const navStyle = {
-          "transform": "translateY(-50%)",
-          "top": "50%",
-          "display": "flex",
-          "alignItems": "center",
-          "flexDirection": "column",
-          "position": "fixed",
-          "border": "1px solid red",
-        }
+          })
 
-        return (
-          <div>
-            <Grid container justify="center">
+          const navStyle = {
+            "transform": "translateY(-50%)",
+            "top": "50%",
+            "display": "flex",
+            "alignItems": "center",
+            "flexDirection": "column",
+            "position": "fixed",
+            "border": "1px solid red",
+          }
 
-              <Grid item xs={12} md={3}>
-                <ProfileViewer />
+          return (
+            <div>
+              <Grid container justify="center">
+
+                <Grid item xs={12} md={3}>
+                  <ProfileViewer />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <Switch>
+                    <Route
+                      exact path="/"
+                      component={() => {
+                        return (
+                          <div>
+                            {contents}
+                          </div>
+                        )
+                      }}
+                    />
+                    <Route path='/*' component={NotFoundViewer} />
+
+                  </Switch>
+                </Grid>
+
+                <Grid item md={3}>
+                  <Hidden smDown implementation="css">
+                    <Grid container>
+                      <div style={navStyle}>
+                        {routes}
+                      </div>
+                    </Grid>
+                  </Hidden>
+                </Grid>
+
               </Grid>
 
-              <Grid item xs={12} md={6}>
-                <Switch>
-                  <Route
-                    exact path="/"
-                    component={() => {
-                      return (
-                        <div>
-                          {contents}
-                        </div>
-                      )
-                    }}
-                  />
-                  <Route path='/*' component={NotFoundViewer} />
+              <StatCounter />
+              <GithubRibbon gitHref="Radivarig" />
 
-                </Switch>
-              </Grid>
-
-              <Grid item md={3}>
-                <Hidden smDown implementation="css">
-                  <Grid container>
-                    <div style={navStyle}>
-                      {routes}
-                    </div>
-                  </Grid>
-                </Hidden>
-              </Grid>
-
-            </Grid>
-
-            <StatCounter />
-            <GithubRibbon gitHref="Radivarig" />
-
-          </div>
-        )
-      }}
-      />
-    </BrowserRouter>
+            </div>
+          )
+        }}
+        />
+      </BrowserRouter>
+    </MuiThemeProvider>
   )
 }
